e9faba052c4cea6df6f1d9e5a4e5bca75f06d108
DanielKovachev <danielkv8@gmail.com>
2019-05-12 00:16:20 +0300

    Second Refactor

--- hw1_part1.py
+++ hw1_part1.py
@@ -33,12 +33,12 @@ def create_unweighted_H_t(train, time):
 
 def create_weighted_H_t(train, time):
 	h_t = nx.Graph()
-	direct_graph = nx.DiGraph(train)
-	for edge in direct_graph.edges():
-		if h_t.has_edge(edge[0], edge[1]) :
-			h_t.add_edge(edge[0], edge[1], weight='strong')
+	graph = nx.DiGraph(train)
+	for e in graph.edges():
+		if h_t.has_edge(e[0], e[1]) :
+			h_t.add_edge(e[0], e[1], weight='strong')
 		else :
-			h_t.add_edge(edge[0], edge[1], weight='weak')
+			h_t.add_edge(e[0], e[1], weight='weak')
 	return h_t
 
 def create_unweighted_G_t(train, time):
@@ -46,85 +46,83 @@ def create_unweighted_G_t(train, time):
 		
 def calc_error(predictions, test, mode='undirected unweighted'):
 	precision, recall=0,0
-	data_list = tuple(map(tuple, test.values.tolist()))
-	intersections = set(predictions) & set(data_list)
-	recall = len(intersections) / len(test)
-	precision = len(intersections) / len(predictions)
+	data = tuple(map(tuple, test.values.tolist()))
+	correct_predictions = set(predictions) & set(data)
+	recall = len(correct_predictions) / len(test)
+	precision = len(correct_predictions) / len(predictions)
 	return (precision, recall)
 
-def update_node_betweenness(node, graph, shortest_paths_length , size):
-	betweenes = 0
+def G_features(G, time):
+	# section a is closeness centrality and b is betweenes centrality
+	a = {}
+	b = {}
+	g_t  = create_unweighted_G_t(G.train, 0)
+	scc = g_t.subgraph(max(nx.strongly_connected_components(g_t), key=len))
+	size = scc.number_of_nodes()
+	shortest_paths_lengths = {}
+	reversed_graph = scc.reverse()
+	for node in  reversed_graph.nodes():
+		shortest_paths_lengths[node] = nx.single_source_shortest_path_length(scc, node)
+		a[node] = closeness_centrality(reversed_graph, node, size)
+	for node in scc.nodes() :
+		b[node] = betweenness_centrality(node, scc, shortest_paths_lengths, size)
+	return {'a': a, 'b': b }
+
+def closeness_centrality(graph, node, size):
+	return (size - 1) / sum(nx.single_source_shortest_path_length(graph,node).values())
+
+def betweenness_centrality(node, graph, shortest_paths_length , size):
+	node_betweenness = 0
 	for x in graph.nodes() :
 		if node != x :
 			for y in graph.nodes() :
 				if x != y and node != y:
-					shortest_paths_with_node = 0
-					shortest_paths = 0
+					paths_with_node = 0
 					shortest_path_length_without_node = shortest_paths_length[x][y]
 					shortest_path_length_with_node = shortest_paths_length[x][node] + shortest_paths_length[node][y]
 					all_shortest_paths = list(nx.all_shortest_paths(graph, x, y))
 					if shortest_path_length_with_node == shortest_path_length_without_node :
 						for path in all_shortest_paths :
 							if node in path :
-								shortest_paths += 1
-								shortest_paths_with_node += 1
-							else:
-								shortest_paths += 1
-					else:
-						shortest_paths += len(list(all_shortest_paths))
-					betweenes += (shortest_paths_with_node / shortest_paths)
-	return (1 / ((size - 1) *(size -2 ))) * betweenes
-
-
-def G_features(G, time):
-	# section a is closeness centrality and b is betweenes centrality
-	g_t  = create_unweighted_G_t(G.train, 0)
-	biggest_scc = nx.DiGraph(max(nx.strongly_connected_component_subgraphs(g_t), key=len))
-	a_dict = {}
-	b_dict = {}
-	size = biggest_scc.number_of_nodes()
-	shortest_paths_lengths = {}
-	reversed_scc = biggest_scc.reverse()
-	for node in  reversed_scc.nodes():
-		shortest_paths_lengths[node] = nx.single_source_shortest_path_length(biggest_scc, node)
-		a_dict[node] = (size - 1) / sum(nx.single_source_shortest_path_length(reversed_scc,node).values())
-
-	for node in biggest_scc.nodes() :
-		b_dict[node] = update_node_betweenness(node, biggest_scc, shortest_paths_lengths, size)
-	
-	return {'a': a_dict, 'b': b_dict }
+								paths_with_node += 1
+					betweenes += (paths_with_node / len(all_shortest_paths))
+	return (1 / ((size - 1) *(size -2 ))) * node_betweenness
 
 def should_add_edge(probability) :
 	return probability > random.random()
-
-def calculate_undirected_weigthed_probability(graph, node, second_node):
-	number_of_strong_connections = 0
-	number_of_weak_connections = 0
-	for neighbor in nx.common_neighbors(graph, node, second_node) :
-		if graph[node][neighbor]['weight'] == 'strong' :
-			number_of_strong_connections += 1
-		else:
-			number_of_weak_connections += 1
-	return 1 - (pow(float(0.96), number_of_strong_connections) * pow(float(0.98),number_of_weak_connections))
 
 def calculate_probabilities(graph, mode='undirected unweighted') :
 	probabilities_to_add_edge = defaultdict(dict)
 	if mode == 'directed':
 		shortest_path_lengths = dict(nx.all_pairs_shortest_path_length(graph))
 	for node in graph.nodes() :
-		for second_node in nx.non_neighbors(graph, node):
+		for non_neighbor in nx.non_neighbors(graph, node):
 			if mode == 'undirected unweighted' :
-				coomon_neighbors_size = len(list(nx.common_neighbors(graph, node, second_node)))
-				probabilities_to_add_edge[node][second_node] = 1 - pow(float(0.97), coomon_neighbors_size)
+				probabilities_to_add_edge[node][non_neighbor] = 1 - pow(float(0.97), len(list(nx.common_neighbors(graph, node, non_neighbor))))
 			elif mode == 'undirected weighted' :
-				probabilities_to_add_edge[node][second_node] = calculate_undirected_weigthed_probability(graph, node, second_node)
+				probabilities_to_add_edge[node][non_neighbor] = calculate_undirected_weigthed_probability(graph, node, non_neighbor)
 			elif mode == 'directed':
-				if second_node in shortest_path_lengths[node]:
-					L = shortest_path_lengths[node][second_node]
-					if L <= 4:
-						M = len(list(nx.all_shortest_paths(graph, node, second_node)))
-						probabilities_to_add_edge[node][second_node] = min(1, M / (math.pow(5, L)))
+				probability = calculate_directed_probability(graph, node, non_neighbor, shortest_path_lengths)
+				if probability is not None:
+					probabilities_to_add_edge[node][non_neighbor] = probability
 	return probabilities_to_add_edge
+
+def calculate_undirected_weigthed_probability(graph, node, non_neighbor):
+	number_of_strong_connections = 0
+	number_of_weak_connections = 0
+	for neighbor in nx.common_neighbors(graph, node, non_neighbor) :
+		if graph[node][neighbor]['weight'] == 'strong' :
+			number_of_strong_connections += 1
+		else:
+			number_of_weak_connections += 1
+	return 1 - (pow(float(0.96), number_of_strong_connections) * pow(float(0.98), number_of_weak_connections))
+
+def calculate_directed_probability(graph, node, second_node, shortest_path_lengths):
+	if second_node in shortest_path_lengths[node]:
+		L = shortest_path_lengths[node][second_node]
+		if L <= 4:
+			M = len(list(nx.all_shortest_paths(graph, node, second_node)))
+			return min(1, M / (math.pow(5, L)))
 
 def run_k_iterations(graph, N, mode='undirected unweighted'):
 	new_edges = []
@@ -142,7 +140,7 @@ def run_k_iterations(graph, N, mode='undirected unweighted'):
 	return new_edges
 	
 
-def partA_q1(dataset):
+def plot_data(dataset):
 	#plot
 	data = pd.read_csv(dataset)
 	df = data[['source', 'target']].groupby('source')['target'].nunique().reset_index(name='count')
@@ -161,14 +159,3 @@ def partA_q1(dataset):
 	ax.set_yscale('log')
 	ax.set_xscale('log')
 	plt.show()
-
-	### bow tie -------------------------------
-	#answer = data['source'].tolist()
-	#ask = data['target'].tolist()
-	#middle = list(set(answer) & set(ask))
-	#left = set(answer) - set(ask)
-	#right = set(ask) - set(answer)
-	#print(len(left))
-	#print(len(middle))
-	#print(len(right))
-	#print(len(left.union(middle, right)))
